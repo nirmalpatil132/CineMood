475e2a35d609df53d6010ec48d4cf8af12823fb0
nirmalpatil132 <156876106+nirmalpatil132@users.noreply.github.com>
2025-08-09 19:25:04 +0530

    Update script.js
    Updated Script

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // CineMood — Real-time TMDB Version
-const TMDB_API_KEY = "35040d62829bedfe15090ba42fa8643a";
+const TMDB_API_KEY = "51bf591a59922b6ce56496a0c32c02fb";
 const platforms = ["netflix", "prime", "jiohotstar", "youtube"];
 const allGenres = {};
 let initialMediaLoad = [];
